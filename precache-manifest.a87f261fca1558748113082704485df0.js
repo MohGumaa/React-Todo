630cf5dae64c48733d15b9feb49abcc9f4b084f7
MohGumaa <gomaam14@gmail.com>
2020-07-18 21:22:44 +0530

    Updates

---
+++ precache-manifest.a87f261fca1558748113082704485df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da311f3ae18a2535ee0056ff3c372952",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "60219556b03cdc20cfca",
+    "url": "/React-Todo/static/css/main.6510979f.chunk.css"
+  },
+  {
+    "revision": "dad1d43ae48f3dee0fc0",
+    "url": "/React-Todo/static/js/2.98633e61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Todo/static/js/2.98633e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60219556b03cdc20cfca",
+    "url": "/React-Todo/static/js/main.735774ad.chunk.js"
+  },
+  {
+    "revision": "025134db386e5b99ec14",
+    "url": "/React-Todo/static/js/runtime-main.1d32c2ae.js"
+  }
+]);
